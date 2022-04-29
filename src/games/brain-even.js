221876dba2d00ba0de29fa68c8cbe047eee30a65
vladyslav hlynianyi <glinyany.lie@gmail.com>
2022-04-30 01:08:56 +0500

    replace random func by lodash usage

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,12 +1,12 @@
+import _ from 'lodash';
 import startBrainGame from '../index.js';
 
 const rulesOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const isEven = (n) => n % 2 === 0;
 
 const generateRound = () => {
-  const randomNumber = getRandomInt(1, 50);
+  const randomNumber = _.random(1, 50);
   const question = `${randomNumber}`;
   const answer = isEven(randomNumber) ? 'yes' : 'no';
   return [question, answer];
