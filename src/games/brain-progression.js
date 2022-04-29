44fd98af89728e20b1ec5cb5367cca0c862247d7
vladyslav hlynianyi <glinyany.lie@gmail.com>
2022-04-29 22:38:10 +0500

    fix logic in brain-progression module

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,29 +2,28 @@ import _ from 'lodash';
 import startBrainGame from '../index.js';
 
 const rulesOfGame = 'What number is missing in the progression?';
-let valueOfItem;
 
 const generateProgression = (startItem, stepItem) => {
   const progressionArray = [];
   const progressionLength = 10;
-  const hiddenItemIndex = _.random(1, 9);
   for (let i = 0; i < progressionLength - 1; i += 1) {
     if (i === 0) {
       progressionArray.push(startItem);
     }
     progressionArray.push(progressionArray[i] + stepItem);
   }
-  valueOfItem = progressionArray[hiddenItemIndex];
-  progressionArray[hiddenItemIndex] = '..';
-  const arrToPrint = progressionArray.join(' ');
-  return arrToPrint;
+  return progressionArray;
 };
 
 const generateRound = () => {
-  const randomStarterInt = _.random(1, 20);
-  const randomHowBigStep = _.random(2, 9);
-  const question = generateProgression(randomStarterInt, randomHowBigStep);
-  const answer = String(valueOfItem);
+  const starterInt = _.random(1, 20);
+  const howBigStep = _.random(2, 9);
+  const hiddenItemIndex = _.random(1, 9);
+  const progression = generateProgression(starterInt, howBigStep);
+  const hiddenItemValue = progression[hiddenItemIndex];
+  progression[hiddenItemIndex] = '..';
+  const question = progression.join(' ');
+  const answer = String(hiddenItemValue);
   return [question, answer];
 };
 
